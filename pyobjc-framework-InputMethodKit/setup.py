3699623739fc157caad7eac65343a627b2fde5f6
Ronald Oussoren <ronaldoussoren@mac.com>
2012-11-01 10:01:17 +0100

    Bump version in trunk

---
+++ pyobjc-framework-InputMethodKit/setup.py
@@ -0,0 +1,25 @@
+''' 
+Wrappers for the "InputMethodKit" framework on MacOSX 10.5 or later. The
+interfaces in this framework allow you to develop input methods.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+'''
+from pyobjc_setup import setup
+
+setup(
+    min_os_level='10.5',
+    name='pyobjc-framework-InputMethodKit',
+    version="2.5.0b1",
+    description = "Wrappers for the framework InputMethodKit on Mac OS X",
+    packages = [ "InputMethodKit" ],
+    setup_requires = [
+        'pyobjc-core>=2.5.0b1',
+    ],
+    install_requires = [ 
+        'pyobjc-core>=2.5.0b1',
+        'pyobjc-framework-Cocoa>=2.5.0b1',
+    ],
+)
---
+++ pyobjc-framework-WebKit/setup.py
@@ -0,0 +1,25 @@
+''' 
+Wrappers for the "WebKit" and "JavaScriptCore" frameworks on MacOSX. The
+WebKit framework contains the views and support classes for creating a
+browser. The JavaScriptCore framework implements a JavaScript interpreter.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use these frameworks and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+'''
+from pyobjc_setup import setup
+
+setup(
+    name='pyobjc-framework-WebKit',
+    version="2.5.0b1",
+    description = "Wrappers for the framework WebKit on Mac OS X",
+    packages = [ "WebKit", "JavaScriptCore" ],
+    setup_requires = [
+        'pyobjc-core>=2.5.0b1',
+    ],
+    install_requires = [ 
+        'pyobjc-core>=2.5.0b1',
+        'pyobjc-framework-Cocoa>=2.5.0b1',
+    ],
+)
