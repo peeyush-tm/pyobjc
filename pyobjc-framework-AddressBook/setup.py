4f88f4ade5d4b2c3656840fe8dd320bf2252a427
Ronald Oussoren <ronaldoussoren@mac.com>
2013-01-07 16:24:04 +0100

    Added tag pyobjc-2.5 for changeset f469eb255847

---
+++ pyobjc-framework-AddressBook/setup.py
@@ -0,0 +1,26 @@
+'''
+Wrappers for the "AddressBook" framework on MacOS X. The Address Book is
+a centralized database for contact and other information for people. Appliations
+that make use of the AddressBook framework all use the same database.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+'''
+
+from pyobjc_setup import setup
+
+setup(
+    name='pyobjc-framework-AddressBook',
+    version="2.5",
+    description = "Wrappers for the framework AddressBook on Mac OS X",
+    packages = [ "AddressBook" ],
+    setup_requires = [
+        'pyobjc-core>=2.5',
+    ],
+    install_requires = [
+        'pyobjc-core>=2.5',
+        'pyobjc-framework-Cocoa>=2.5',
+    ],
+)
